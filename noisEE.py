0d6ddfe4ed891668e9c9b63deee4a4239fc416b6
Nathan Hwang <thenoviceoof@gmail.com>
2015-01-02 02:08:01 -0500

    Fetch the wav data

--- noisEE.py
+++ noisEE.py
@@ -4,10 +4,34 @@
 # falloff, in pursuit of noise of any color.
 ################################################################################
 
+import argparse
+import array
 from numpy import fft
+import wave
 
-def main():
-    pass
+def read_wav(path):
+    wav_file = wave.open(path)
+
+    assert wav_file.getnchannels() == 1
+    assert wav_file.getframerate() == 44100, 'Expect 44.1k audio'
+    assert wav_file.getnframes() <= 44100, 'Expect less than a second of audio'
+    assert wav_file.getsampwidth() == 2, 'Expected signed 16 bit audio'
+
+    data_string = wav_file.readframes(wav_file.getnframes())
+
+    # Convert the data from string to byte(s) array
+    data = array.array('h')
+    data.fromstring(data_string)
+
+    return list(data)
+
+def main(wav_path):
+    data = read_wav(wav_path)
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('wavpath', help='')
+
+    args = parser.parse_args()
+
+    main(args.wavpath)
