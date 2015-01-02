61b4fdf9acacc2b3c0231c86160b2561836b0300
Nathan Hwang <thenoviceoof@gmail.com>
2015-01-02 01:45:42 -0500

    Start with a skeleton script

---
+++ noisEE.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+################################################################################
+# Script to find families of digital filter parameters that produce arbitrary
+# falloff, in pursuit of noise of any color.
+################################################################################
+
+from numpy import fft
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
