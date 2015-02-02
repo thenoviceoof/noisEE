8bd6665d9e44e7adae6b8583ffa1f2e7158449fd
Nathan Hwang <thenoviceoof@gmail.com>
2015-02-01 23:34:01 -0500

    Clean up a bit

--- knee.py
+++ knee.py
@@ -36,42 +36,9 @@ def test1(wav_data, white=0.1, steps=20):
 def main(paths, steps=10):
     wav_data = [read_wav(path) for path in paths]
 
-    ########################################
-    # make some filters
-    stops = [float(i)/steps for i in range(steps)]
-    #stops = [(float(steps-i)/steps)**2 for i in range(steps)]
-    #stops = [1./(2**i) for i in range(steps)]
-    
-    # log exchange - equal spacing at const falloff
-    #stops = [1./(4**(steps-i)) for i in range(steps)]
-    #fils = [[1-s, s] for s in stops]
-
-    stops = [float(i)/steps for i in range(steps)]
-    fils = [[1-s, 0.1] for s in stops]
-
-    # Trying things
-    # fils = [(0.96300, 0.296514)]
-
-    ########################################
-    # prep the data
-    # fil_data = [get_lg_data(wav_data, fil) for fil in fils]
-    # xs = fil_data[0][0]
-    # fil_data = [fd[1] for fd in fil_data]
-
-    ########################################
-    # display the data
-    # for fdata in fil_data:
-    #     plt.plot(xs, fdata)
-    # plt.show()
-
     xms, yms = test1(wav_data, white=0.2, steps=20)
     yms = [(y/22050) for y in yms]
-    #yms = [ for y in yms]
-    #plt.plot(xms, yms)
 
-    # yms  = [1/((1.8*x+1)**3) for x in xms]
-    #coeff = 30.
-    #yms = [(coeff**((1-x)) - 1)/(coeff-1.) for x in xms]
     xms = [1.-x for x in xms]
     plt.plot(xms, yms)
     res = numpy.polyfit(xms, yms, 4, full=True)
