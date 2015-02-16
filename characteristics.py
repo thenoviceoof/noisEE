b1bf074ef16725678e77e72781e76b7f5ec1e85d
Nathan Hwang <thenoviceoof@gmail.com>
2015-02-16 17:28:08 -0500

    Gather characteristics results work into one place

---
+++ characteristics.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+################################################################################
+# Gather characteristics work in one place
+
+import argparse
+import math
+
+from libnoisEE import *
+
+def filter_characteristics(white, state):
+    knee_frac = (0.00022856304700783622
+                 + 0.41420297628235303 * state
+                 - 0.42118671936182317 * state**2
+                 + 0.42118671936182317 * state**3
+                 + 0.892743444761949 * state**4)
+    knee_freq = (SAMPLE_RATE/2) * (1 - knee_frac)
+    pass_band = (6.02059991328 * math.log(white, 2)
+                 + 14.036948327785755 * state
+                 - 22.850386042107317 * state**2
+                 + 34.797524550144153 * state**3)
+
+    assert knee_freq <= SAMPLE_RATE/2
+    assert knee_freq >= 0
+
+    return (knee_freq,pass_band)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-w', '--white', type=float, default=1.0)
+    parser.add_argument('-s', '--state', type=float, default=0.0)
+
+    args = parser.parse_args()
+
+    knee_freq, pass_band = filter_characteristics(args.white, args.state)
+    print (knee_freq, pass_band)
