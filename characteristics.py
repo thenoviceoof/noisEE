a154c097f299b93eef1d791be8b5a2eec48500d2
Nathan Hwang <thenoviceoof@gmail.com>
2015-02-17 23:20:59 -0500

    Make some changes based on bugs found while poking around

--- characteristics.py
+++ characteristics.py
@@ -8,19 +8,19 @@ import math
 from libnoisEE import *
 
 def filter_characteristics(white, state):
+    rev_state = 1 - state
     knee_frac = (0.00022856304700783622
-                 + 0.41420297628235303 * state
-                 - 0.42118671936182317 * state**2
-                 + 0.42118671936182317 * state**3
-                 + 0.892743444761949 * state**4)
-    knee_freq = (SAMPLE_RATE/2) * (1 - knee_frac)
+                 + 0.41420297628235303 * rev_state
+                 - 0.42118671936182317 * rev_state**2
+                 + 0.42118671936182317 * rev_state**3
+                 + 0.892743444761949 * rev_state**4)
+    knee_freq = (SAMPLE_RATE/2) * knee_frac
     pass_band = (6.02059991328 * math.log(white, 2)
                  + 14.036948327785755 * state
                  - 22.850386042107317 * state**2
                  + 34.797524550144153 * state**3)
 
-    assert knee_freq <= SAMPLE_RATE/2
-    assert knee_freq >= 0
+    assert knee_freq >= 0, knee_freq
 
     return (knee_freq,pass_band)
 
